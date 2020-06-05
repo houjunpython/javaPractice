1582c75c270e2f6137cfbffbb7a104b26eeebff8
houjunpython <1581465525@qq.com>
2020-06-05 23:40:47 +0800

    Initial commit

---
+++ src/FirstCode/HelloWorld.java
@@ -0,0 +1,10 @@
+package FirstCode;
+public class HelloWorld {
+    public HelloWorld() {
+      System.out.print("hello world!");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        HelloWorld hl=new HelloWorld();
+    }
+}
